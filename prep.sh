a316e7d4e6f3bdd09c3ed18bdab50dcbd85ade77
eladeyal-intel <elad.eyal@intel.com>
2021-01-02 16:47:59 +0200

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.vim && curl -sSL https://www.vim.org/scripts/download_script.php?src
 git clone --depth=1 https://tpope.io/vim/fugitive.git ~/.vim/pack/tpope/start/fugitive && rm -fr ~/.vim/pack/tpope/start/fugitive/.git
 git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline && rm -fr ~/.vim/pack/dist/start/vim-airline/.git
 #git clone --depth=1 https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes && rm -fr ~/.vim/pack/dist/start/vim-airline-themes/.git
-curl -sSL https://www.vim.org/scripts/download_script.php?src_id=14498 | gunzip -f > /tmp/AnsiEsc.vba && vim -c ":so /tmp/AnsiEsc.vba" -c q && rm /tmp/AnsiEsc.vba
+curl -sSL https://www.vim.org/scripts/download_script.php?src_id=14498 | gunzip -f > /tmp/AnsiEsc.vba && vim --not-a-term -c ":so /tmp/AnsiEsc.vba" -c q && rm /tmp/AnsiEsc.vba
 vim --not-a-term -u NONE -c "helptags ~/.vim/pack/tpope/start/fugitive/doc" -c "helptags ~/.vim/pack/dist/start/vim-airline/doc" -c q
 
 tmux source-file ~/.tmux.conf || true
