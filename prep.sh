2dfc3b37fb704363debf759b4980481ad3fc0418
eladeyal-intel <elad.eyal@intel.com>
2021-01-31 19:51:04 +0200

    typo

--- prep.sh
+++ prep.sh
@@ -6,8 +6,8 @@ curl -sSL https://codeload.github.com/eladeyal-intel/profile/tar.gz/master? | ta
 mkdir -p ~/.lnav && [[ ! -L ~/.lnav/stdin-captures ]] && rm -fr ~/.lnav/stdin-captures && ln -s /tmp ~/.lnav/stdin-captures
 
 mkdir -p ~/.vim && curl -sSL https://www.vim.org/scripts/download_script.php?src_id=26208 | tar zx --strip=1 -C ~/.vim vim-sahara-0.4.0/colors
-mkdir -p ~/.vim/pack/tpope/start && curl -sSL https://codeload.github.com/tpope/vim-fugitive/tar.gz/master | tar xzf -C ~/.vim/pack/tpope/start
-mkdir -p ~/.vim/pack/dist/start && curl -sSL https://codeload.github.com/vim-airline/vim-airline/tar.gz/master tar | zxf -C  ~/.vim/pack/dist/start
+mkdir -p ~/.vim/pack/tpope/start && curl -sSL https://codeload.github.com/tpope/vim-fugitive/tar.gz/master | tar xz -C ~/.vim/pack/tpope/start
+mkdir -p ~/.vim/pack/dist/start && curl -sSL https://codeload.github.com/vim-airline/vim-airline/tar.gz/master tar | zx -C  ~/.vim/pack/dist/start
 #git clone --depth=1 https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes && rm -fr ~/.vim/pack/dist/start/vim-airline-themes/.git
 curl -sSL https://www.vim.org/scripts/download_script.php?src_id=14498 | gunzip -f > /tmp/AnsiEsc.vba && vim --not-a-term -c ":so /tmp/AnsiEsc.vba" -c q && rm /tmp/AnsiEsc.vba
 vim --not-a-term -u NONE -c "helptags ~/.vim/pack/tpope/start/fugitive/doc" -c "helptags ~/.vim/pack/dist/start/vim-airline/doc" -c q
